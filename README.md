44df4d2ae7c4d0e9f1939453d2b26fef61ee719c
bavar <8374958+bavar@users.noreply.github.com>
2026-01-25 18:59:04 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # de-zoomcamp
 Docker workshop using codespaces
+
+Module 1 Homework: Docker & SQL
---
+++ pipeline/ingest_data.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+def main():
+    engine = create_engine('postgresql://postgres:postgres@localhost:5433/ny_taxi')
+
+    #'./data/green_tripdata_2025-11.parquet'
+    df = pd.read_parquet(
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet',
+        engine='pyarrow'
+    )
+
+    df.to_sql(
+        name='green_taxi_data',
+        con=engine,
+        if_exists='replace'
+    )
+
+    #'./data/taxi_zone_lookup.csv'
+    df_iter = pd.read_csv(
+        'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv',
+        iterator=True,
+        chunksize=100000,
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name='taxi_zones',
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name='taxi_zones',
+            con=engine,
+            if_exists='append'
+        )
+
+if __name__ == "__main__":
+    main()
